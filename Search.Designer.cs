4f2f6f6c11b9fb67ad746d589245aad87ba9f736
Dmitry Loginov <interestingfacts223@gmail.com>
2024-03-29 02:13:52 +0300

    added parse dir

--- Search.Designer.cs
+++ Search.Designer.cs
@@ -29,25 +29,28 @@ namespace Search_Tool
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Search));
             this.search_btn = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.keyword_box = new System.Windows.Forms.TextBox();
+            this.choose_folder_dialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.choose_start_dir = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.path = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.selected_path = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // search_btn
             // 
             this.search_btn.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.search_btn.Location = new System.Drawing.Point(12, 80);
+            this.search_btn.Location = new System.Drawing.Point(14, 305);
             this.search_btn.Name = "search_btn";
-            this.search_btn.Size = new System.Drawing.Size(239, 50);
+            this.search_btn.Size = new System.Drawing.Size(497, 50);
             this.search_btn.TabIndex = 0;
             this.search_btn.Text = "Поиск";
             this.search_btn.UseVisualStyleBackColor = true;
+            this.search_btn.Click += new System.EventHandler(this.search_btn_Click);
             // 
             // label1
             // 
@@ -64,44 +67,66 @@ namespace Search_Tool
             this.keyword_box.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.keyword_box.Location = new System.Drawing.Point(12, 35);
             this.keyword_box.Name = "keyword_box";
-            this.keyword_box.Size = new System.Drawing.Size(239, 30);
+            this.keyword_box.Size = new System.Drawing.Size(497, 30);
             this.keyword_box.TabIndex = 2;
+            // 
+            // choose_folder_dialog
+            // 
+            this.choose_folder_dialog.HelpRequest += new System.EventHandler(this.choose_folder_dialog_HelpRequest);
+            // 
+            // choose_start_dir
+            // 
+            this.choose_start_dir.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.choose_start_dir.Location = new System.Drawing.Point(13, 218);
+            this.choose_start_dir.Name = "choose_start_dir";
+            this.choose_start_dir.Size = new System.Drawing.Size(497, 71);
+            this.choose_start_dir.TabIndex = 0;
+            this.choose_start_dir.Text = "Выберите начальную директорию";
+            this.choose_start_dir.UseVisualStyleBackColor = true;
+            this.choose_start_dir.Click += new System.EventHandler(this.choose_start_dir_Click);
             // 
             // dataGridView1
             // 
-            this.dataGridView1.AllowUserToAddRows = false;
             this.dataGridView1.AllowUserToDeleteRows = false;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.path});
-            this.dataGridView1.Location = new System.Drawing.Point(286, 35);
+            this.dataGridView1.Location = new System.Drawing.Point(515, 35);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
             this.dataGridView1.Size = new System.Drawing.Size(502, 403);
             this.dataGridView1.TabIndex = 3;
             // 
-            // path
+            // selected_path
             // 
-            this.path.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle1.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.path.DefaultCellStyle = dataGridViewCellStyle1;
-            this.path.FlatStyle = System.Windows.Forms.FlatStyle.System;
-            this.path.HeaderText = "Путь";
-            this.path.Name = "path";
-            this.path.ReadOnly = true;
-            this.path.Resizable = System.Windows.Forms.DataGridViewTriState.True;
-            this.path.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            this.selected_path.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.selected_path.Location = new System.Drawing.Point(13, 99);
+            this.selected_path.Multiline = true;
+            this.selected_path.Name = "selected_path";
+            this.selected_path.ReadOnly = true;
+            this.selected_path.Size = new System.Drawing.Size(496, 113);
+            this.selected_path.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(13, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(195, 23);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Поиск в директории:";
             // 
             // Search
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(1029, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.selected_path);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.keyword_box);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.choose_start_dir);
             this.Controls.Add(this.search_btn);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Search";
@@ -117,8 +142,11 @@ namespace Search_Tool
         private System.Windows.Forms.Button search_btn;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox keyword_box;
-        private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.DataGridViewButtonColumn path;
+        private System.Windows.Forms.FolderBrowserDialog choose_folder_dialog;
+        private System.Windows.Forms.Button choose_start_dir;
+        private System.Windows.Forms.TextBox selected_path;
+        private System.Windows.Forms.Label label2;
+        public System.Windows.Forms.DataGridView dataGridView1;
     }
 }
 
--- Search.cs
+++ Search.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,41 @@ namespace Search_Tool
         public Search()
         {
             InitializeComponent();
+        }
+
+        private void search_btn_Click(object sender, EventArgs e)
+        {
+            string[] dir_data = ParseDirectory(selected_path.Text);
+            dataGridView1.RowCount = dir_data.Length;
+            dataGridView1.ColumnCount = 1;
+            for (int i = 0; i < dir_data.Length - 1; i++)
+            {
+                dataGridView1.Rows[i].Cells[0].Value = dir_data[i];
+            }
+            dataGridView1.Refresh();
+            dataGridView1.Update();
+        }
+
+        public string[] ParseDirectory(string path)
+        {
+            if (!Directory.Exists(path)) return new string[] { };
+
+            return Directory.GetFileSystemEntries(path);
+        }
+
+        private void choose_start_dir_Click(object sender, EventArgs e)
+        {
+            choose_folder_dialog.Description = "Выберите директорию, в которой будет начинаться поиск";
+            choose_folder_dialog.ShowNewFolderButton = true;
+            if (choose_folder_dialog.ShowDialog() == DialogResult.OK)
+            {
+                selected_path.Text = choose_folder_dialog.SelectedPath;
+            }
+        }
+
+        private void choose_folder_dialog_HelpRequest(object sender, EventArgs e)
+        {
+
         }
     }
 }
