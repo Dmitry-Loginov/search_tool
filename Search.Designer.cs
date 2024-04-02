3494da5e083b8f9c8cd53b79c7013960fe7606d7
dmitry.loginov <interestingfacts223@gmail.com>
2024-04-02 20:50:29 +0300

    added exports and open items

--- Search.Designer.cs
+++ Search.Designer.cs
@@ -29,6 +29,7 @@ namespace Search_Tool
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Search));
             this.search_btn = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
@@ -36,18 +37,25 @@ namespace Search_Tool
             this.choose_folder_dialog = new System.Windows.Forms.FolderBrowserDialog();
             this.choose_start_dir = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.open_cmenu_item = new System.Windows.Forms.ToolStripMenuItem();
             this.selected_path = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.ignore_case = new System.Windows.Forms.CheckBox();
             this.deep_search = new System.Windows.Forms.CheckBox();
             this.regexp_mode = new System.Windows.Forms.CheckBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.помощьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.export_result_cmenu_item = new System.Windows.Forms.ToolStripMenuItem();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // search_btn
             // 
             this.search_btn.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.search_btn.Location = new System.Drawing.Point(15, 477);
+            this.search_btn.Location = new System.Drawing.Point(15, 499);
             this.search_btn.Margin = new System.Windows.Forms.Padding(4);
             this.search_btn.Name = "search_btn";
             this.search_btn.Size = new System.Drawing.Size(663, 62);
@@ -60,7 +68,7 @@ namespace Search_Tool
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(12, 11);
+            this.label1.Location = new System.Drawing.Point(12, 33);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(307, 29);
@@ -70,20 +78,17 @@ namespace Search_Tool
             // keyword_box
             // 
             this.keyword_box.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.keyword_box.Location = new System.Drawing.Point(16, 43);
+            this.keyword_box.Location = new System.Drawing.Point(16, 65);
             this.keyword_box.Margin = new System.Windows.Forms.Padding(4);
             this.keyword_box.Name = "keyword_box";
             this.keyword_box.Size = new System.Drawing.Size(661, 36);
             this.keyword_box.TabIndex = 2;
             this.keyword_box.TextChanged += new System.EventHandler(this.keyword_box_TextChanged);
             // 
-            // choose_folder_dialog
-            // 
-            // 
             // choose_start_dir
             // 
             this.choose_start_dir.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.choose_start_dir.Location = new System.Drawing.Point(13, 370);
+            this.choose_start_dir.Location = new System.Drawing.Point(13, 392);
             this.choose_start_dir.Margin = new System.Windows.Forms.Padding(4);
             this.choose_start_dir.Name = "choose_start_dir";
             this.choose_start_dir.Size = new System.Drawing.Size(663, 87);
@@ -99,18 +104,38 @@ namespace Search_Tool
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.ContextMenuStrip = this.contextMenuStrip1;
             this.dataGridView1.Location = new System.Drawing.Point(687, 43);
             this.dataGridView1.Margin = new System.Windows.Forms.Padding(4);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
             this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.Size = new System.Drawing.Size(669, 496);
+            this.dataGridView1.Size = new System.Drawing.Size(669, 548);
             this.dataGridView1.TabIndex = 3;
+            this.dataGridView1.SelectionChanged += new System.EventHandler(this.dataGridView1_SelectionChanged);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.open_cmenu_item,
+            this.export_result_cmenu_item});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(211, 80);
+            this.contextMenuStrip1.Click += new System.EventHandler(this.contextMenuStrip1_Click);
+            // 
+            // open_cmenu_item
+            // 
+            this.open_cmenu_item.Enabled = false;
+            this.open_cmenu_item.Name = "open_cmenu_item";
+            this.open_cmenu_item.Size = new System.Drawing.Size(210, 24);
+            this.open_cmenu_item.Text = "Открыть";
+            this.open_cmenu_item.ToolTipText = "Открыть выбранный элемент";
             // 
             // selected_path
             // 
             this.selected_path.Font = new System.Drawing.Font("Tahoma", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.selected_path.Location = new System.Drawing.Point(13, 224);
+            this.selected_path.Location = new System.Drawing.Point(13, 246);
             this.selected_path.Margin = new System.Windows.Forms.Padding(4);
             this.selected_path.Multiline = true;
             this.selected_path.Name = "selected_path";
@@ -123,7 +148,7 @@ namespace Search_Tool
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label2.Location = new System.Drawing.Point(13, 191);
+            this.label2.Location = new System.Drawing.Point(13, 213);
             this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(246, 29);
@@ -135,7 +160,7 @@ namespace Search_Tool
             this.ignore_case.AutoSize = true;
             this.ignore_case.Checked = true;
             this.ignore_case.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.ignore_case.Location = new System.Drawing.Point(18, 86);
+            this.ignore_case.Location = new System.Drawing.Point(18, 108);
             this.ignore_case.Name = "ignore_case";
             this.ignore_case.Size = new System.Drawing.Size(242, 20);
             this.ignore_case.TabIndex = 6;
@@ -148,7 +173,7 @@ namespace Search_Tool
             this.deep_search.AutoSize = true;
             this.deep_search.Checked = true;
             this.deep_search.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.deep_search.Location = new System.Drawing.Point(267, 87);
+            this.deep_search.Location = new System.Drawing.Point(267, 109);
             this.deep_search.Name = "deep_search";
             this.deep_search.Size = new System.Drawing.Size(123, 20);
             this.deep_search.TabIndex = 7;
@@ -158,19 +183,43 @@ namespace Search_Tool
             // regexp_mode
             // 
             this.regexp_mode.AutoSize = true;
-            this.regexp_mode.Location = new System.Drawing.Point(18, 113);
+            this.regexp_mode.Location = new System.Drawing.Point(18, 135);
             this.regexp_mode.Name = "regexp_mode";
             this.regexp_mode.Size = new System.Drawing.Size(234, 20);
             this.regexp_mode.TabIndex = 8;
             this.regexp_mode.Text = "Режим регулярного выражения";
             this.regexp_mode.UseVisualStyleBackColor = true;
             // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.помощьToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1372, 28);
+            this.menuStrip1.TabIndex = 9;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // помощьToolStripMenuItem
+            // 
+            this.помощьToolStripMenuItem.Name = "помощьToolStripMenuItem";
+            this.помощьToolStripMenuItem.ShortcutKeys = System.Windows.Forms.Keys.F1;
+            this.помощьToolStripMenuItem.Size = new System.Drawing.Size(110, 24);
+            this.помощьToolStripMenuItem.Text = "Помощь   F1";
+            // 
+            // export_result_cmenu_item
+            // 
+            this.export_result_cmenu_item.Name = "export_result_cmenu_item";
+            this.export_result_cmenu_item.Size = new System.Drawing.Size(210, 24);
+            this.export_result_cmenu_item.Text = "Выгрузить отчет";
+            // 
             // Search
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoScroll = true;
-            this.ClientSize = new System.Drawing.Size(1372, 554);
+            this.ClientSize = new System.Drawing.Size(1372, 606);
             this.Controls.Add(this.regexp_mode);
             this.Controls.Add(this.deep_search);
             this.Controls.Add(this.ignore_case);
@@ -181,12 +230,17 @@ namespace Search_Tool
             this.Controls.Add(this.label1);
             this.Controls.Add(this.choose_start_dir);
             this.Controls.Add(this.search_btn);
+            this.Controls.Add(this.menuStrip1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
             this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Search";
             this.Text = "Search Tool";
             this.Load += new System.EventHandler(this.Search_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -205,6 +259,11 @@ namespace Search_Tool
         private System.Windows.Forms.CheckBox ignore_case;
         private System.Windows.Forms.CheckBox deep_search;
         private System.Windows.Forms.CheckBox regexp_mode;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem помощьToolStripMenuItem;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem open_cmenu_item;
+        private System.Windows.Forms.ToolStripMenuItem export_result_cmenu_item;
     }
 }
 
--- Search.cs
+++ Search.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using OfficeOpenXml;
+using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
@@ -21,21 +23,41 @@ namespace Search_Tool
 
         public Regex Regex { get; set; }
 
+        public string[] SearchResult { get; set; }
+
         private void search_btn_Click(object sender, EventArgs e)
         {
             if (regexp_mode.Checked) Regex = CreateRegexByKeywords();
-            if (RegexpError) return;
+            if (RegexpError)
+            { 
+                dataGridView1.Rows.Clear();
 
-            string[] dir_data = ParseDirectory(selected_path.Text);
+                for (int i = 0; i < contextMenuStrip1.Items.Count; i++)
+                {
+                    contextMenuStrip1.Items[i].Enabled = false;
+                }
 
-            var result = dir_data.Where(data => 
+                return;
+            }
+
+            SearchResult = ParseDirectory(selected_path.Text);
+
+            var result = SearchResult.Where(data => 
                 {
                     bool t =  CheckByKeyword(data);
                     return t;
                 }
             ).ToArray();
 
-            if (result.Length == 0) { dataGridView1.Rows.Clear(); return; } ;
+            if (result.Length == 0)
+            {
+                dataGridView1.Rows.Clear();
+                for (int i = 0; i < contextMenuStrip1.Items.Count; i++)
+                {
+                    contextMenuStrip1.Items[i].Enabled = false;
+                }
+                return;
+            }
 
             dataGridView1.RowCount = result.Length;
             dataGridView1.ColumnCount = 1;
@@ -47,6 +69,11 @@ namespace Search_Tool
             dataGridView1.Columns[0].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
             dataGridView1.Refresh();
             dataGridView1.Update();
+            SelectedItem = dataGridView1.SelectedCells[0].Value.ToString();
+            for (int i = 0; i < contextMenuStrip1.Items.Count; i++)
+            {
+                contextMenuStrip1.Items[i].Enabled = true;
+            }
         }
 
         public string[] ParseDirectory(string path)
@@ -68,6 +95,8 @@ namespace Search_Tool
                     FileInfo fileInfo = new FileInfo(entry);
                     if ((fileInfo.Attributes & FileAttributes.System) == FileAttributes.System &&
                 (fileInfo.Attributes & FileAttributes.Hidden) == FileAttributes.Hidden) continue;
+
+                    files.Add(entry);
                     files.AddRange(ParseDirectory(entry));
                 }
             }
@@ -120,6 +149,8 @@ namespace Search_Tool
             selected_path.Text = Directory.GetCurrentDirectory();
             SearchedText = "";
             RegexpError = false;
+            open_cmenu_item.Click += OpenClick;
+            export_result_cmenu_item.Click += ExportClick;
         }
 
         private void keyword_box_TextChanged(object sender, EventArgs e)
@@ -130,6 +161,106 @@ namespace Search_Tool
         private void ignore_case_CheckedChanged(object sender, EventArgs e)
         {
             SearchedText = ignore_case.Checked ? keyword_box.Text.ToLower() : keyword_box.Text;
+        }
+
+        private void dataGridView1_SelectionChanged(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedCells.Count == 0 || dataGridView1.SelectedCells[0].Value == null) return;
+            SelectedItem = dataGridView1.SelectedCells[0].Value.ToString();
+        }
+
+        public string SelectedItem { get; set; }
+
+        private void contextMenuStrip1_Click(object sender, EventArgs e)
+        {
+            contextMenuStrip1.Hide();
+        }
+
+        public void ExportClick(object sender, EventArgs e)
+        {
+            string searchResult = $"Результат поиска: {keyword_box.Text}";
+
+            int fileCount = 0;
+            int folderCount = 0;
+            int totalCount = SearchResult.Length;
+
+            // Создаем новый пакет Excel
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+            using (ExcelPackage package = new ExcelPackage())
+            {
+                // Добавляем лист
+                ExcelWorksheet worksheet = package.Workbook.Worksheets.Add("Данные");
+
+                // Заполняем столбец A данными
+                for (int i = 0; i < SearchResult.Length; i++)
+                {
+                    worksheet.Cells[i + 2, 1].Value = SearchResult[i];
+                    if (File.Exists(SearchResult[i]))
+                    {
+                        fileCount++;
+                    }
+                    else if (Directory.Exists(SearchResult[i]))
+                    {
+                        folderCount++;
+                    }
+                }
+
+                // Добавляем заголовок столбца
+                worksheet.Cells[1, 1].Value = searchResult;
+
+                var date_time_export = DateTime.Now.ToString().Replace(':', '_');
+
+                // Добавляем таблицу со статистикой
+                worksheet.Cells[1, 3].Value = "Дата и время выгрузки отчета:";
+                worksheet.Cells[1, 4].Value = date_time_export.Replace('_', ':');
+                worksheet.Cells[2, 3].Value = "Количество файлов:";
+                worksheet.Cells[2, 4].Value = fileCount;
+                worksheet.Cells[3, 3].Value = "Количество папок:";
+                worksheet.Cells[3, 4].Value = folderCount;
+                worksheet.Cells[4, 3].Value = "Общее количество элементов:";
+                worksheet.Cells[4, 4].Value = totalCount;
+
+                // Получаем все используемые колонки
+                int startColumn = worksheet.Dimension.Start.Column;
+                int endColumn = worksheet.Dimension.End.Column;
+
+                // Проходим по каждой колонке и делаем их шире
+                for (int col = startColumn; col <= endColumn; col++)
+                {
+                    worksheet.Column(col).AutoFit();
+                }
+
+                // Сохраняем файл Excel на диск
+                FileInfo file = new FileInfo($"результат {date_time_export}.xlsx");
+                package.SaveAs(file);
+            }
+
+            Console.WriteLine("Файл Excel успешно создан.");
+            Console.ReadLine();
+        }
+
+        public void OpenClick(object sender, EventArgs e)
+        {
+            if (!contextMenuStrip1.Items[0].Enabled) return;
+
+            bool isDirectory = Directory.Exists(SelectedItem);
+            bool isFile = File.Exists(SelectedItem);
+
+            if (isDirectory || isFile)
+            {
+                try
+                {
+                    Process.Start(SelectedItem);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Ошибка: {ex.Message}", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show($"Указанный путь не существует", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
